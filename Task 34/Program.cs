1167ad9d1a455e9c3bf31c21bc4c7e0a95bd05a9
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-05 19:28:15 +0700

    Task 34 added

---
+++ Task 34/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillRandomValuesInArray(ref int[] NumsArray)
+{
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+    {
+        NumsArray[i] = new Random().Next(100, 1000);
+    }    
+    
+}
+
+int GetEvenNumsCount(int[] NumsArray)
+{
+    int Result = 0;
+
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+    {
+        if (NumsArray[i] % 2 == 0) Result++;
+    }
+
+    return Result;        
+}
+
+void PrintArray(int[] NumsArray)
+{
+    int ArraySize = NumsArray.Length;
+    
+    Console.Write("[");
+    
+    for (int i = 0; i < ArraySize; i++)
+    {
+        Console.Write(NumsArray[i]);
+        if (i < ArraySize - 1) Console.Write(", ");
+    }
+    
+    Console.Write("]");
+}
+
+int SIZE = 8;
+
+int[] NumsArray = new int[SIZE];
+
+FillRandomValuesInArray(ref NumsArray);
+PrintArray(NumsArray);
+
+Console.WriteLine();
+
+Console.WriteLine($"Count of even numbers in the array: {GetEvenNumsCount(NumsArray)}");
