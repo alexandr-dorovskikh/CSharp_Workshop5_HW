6ee398dbd473dfd01b2730ec9b5b08f0773dc2f8
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-12-05 20:27:56 +0700

    Task 38 added

---
+++ Task 38/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillRandomValuesInArray(ref double[] NumsArray)
+{
+    for (int i = 0; i < NumsArray.GetLength(0); i++)
+    {
+        NumsArray[i] = new Random().NextDouble() + new Random().Next(0, 100);
+    }    
+    
+}
+
+double DiffOfMinAndMaxNums(double[] NumsArray)
+{
+    return NumsArray.Max() - NumsArray.Min();          
+}
+
+void PrintArray(double[] NumsArray)
+{
+    Console.Write($"[{string.Join(", ", NumsArray)}]");
+}
+
+int SIZE = 8;
+
+double[] NumsArray = new double[SIZE];
+
+FillRandomValuesInArray(ref NumsArray);
+PrintArray(NumsArray);
+
+Console.WriteLine();
+
+Console.WriteLine($"Diff of max & min nums in the array: {DiffOfMinAndMaxNums(NumsArray)}");
